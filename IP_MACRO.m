282f1afd0c48068db61a89181b8eb3b3ae5af1ba
GabrieLeitao <gleitao147@gmail.com>
2026-03-11 21:34:21 +0000

    Simluting dead zone, noise, sensor quantization, saturation, motor lag

--- IP_MACRO.m
+++ IP_MACRO.m
@@ -13,11 +13,11 @@ ALPHA_MAX = 90 * pi/180; % Maximum angle accepted for motor shaft
 BETA_MAX = 15 * pi/180; % Maximum angle before falling
 TIME_DELAY = 6; % seconds before start
 
-T = 5; % Time duration of the simulation
+T = 12; % Time duration of the simulation
 Ts = 0.001; % Sampling time
 
 % Regulator parameters
-Qr = diag([10,0,1,0,0]); %Weight Matrix for x
+Qr = diag([10,1,5,1,0]); %Weight Matrix for x
 Rr = 0.1; %Weight for the input variable
 K = lqr(A, B, Qr, Rr); %Calculate feedback gain
 
@@ -28,24 +28,35 @@ Re = eye(2); %Variance of measurement errors
 L = lqe(A, G, C, Qe, Re); %Calculate estimator gains
 
 
+% Solve the continuous-time algebraic Riccati equation for estimator
+Pe = care(A', C', Qe, Re);  % Note transpose of A and C
+J_estimator = trace(Pe);  % Scalar measure of overall estimation error
+
 % Furuta pendulum - State feedback test 
 %---------------------------------------------------------------------- 
 % Simulate controller 
-x0=[0.1 1 0.2 0 0]'; 
+x0=[0.1 0.5 0.2 0 0]; 
 D=[0 0]';
 
-D_perturb = [0 0 0 0 0]';
-C_perturb = eye(5);
-Rep = eye(5);
-L_perturb = lqe(A, G, C_perturb, Qe, Rep); %Calculate estimator gains
+% Sim parameters
+saturation = 5; % -5 to 5
+tau = 0.02; % Motor lag: 0.02 - 0.1s
+dead_zone = 0.1; % Dead zone half-width
 
+%b_alpha = 0.001; % Viscous friction for alpha: 0.001 - 0.01 
+%b_beta = 0.0005; % Viscous friction for beta: 0.0005 - 0.005 
+%tau_c = 0.01; % Coulomb friction: 0.01 - 0.05 N.m
 
-sim('LQE_perturbed.slx',T); 
-gg=plot(t.Time,y.Data(:,2)); 
+noise_power = 1e-5; % White noise power
+noise_sample_time = 0.01; % s
+quant = 2 * pi / 4096; % quantizer interval in output
+
+sim('LQG.slx',T);
+gg=plot(t.Time,y.Data(:,2));
 set(gg,'LineWidth',1.5) 
-gg=xlabel('Time (s9'); 
-set(gg,'Fontsize',14); 
-gg=ylabel('\beta (rad)'); 
+gg=xlabel('Time (s)');
+set(gg,'Fontsize',14);
+gg=ylabel('\beta (rad)');
 set(gg,'Fontsize',14); 
 
 
@@ -53,6 +64,18 @@ set(gg,'Fontsize',14);
 t     = y.Time;         % time vector
 alpha = y.Data(:,1);    % alpha signal
 beta  = y.Data(:,2);    % beta signal
+
+MSE_alpha = (1/T) * trapz(t, alpha.^2);
+MSE_beta = (1/T) * trapz(t, beta.^2);
+Energy = trapz(t, u.Data(:,1).^2);
+
+fprintf("MSE_alpha: %g\n", MSE_alpha);
+fprintf("MSE_beta: %g\n", MSE_beta);
+fprintf("Energy: %g\n", Energy);
+
+%% Display results
+disp('Computed Rr:'), disp(Rr)
+disp('Feedback gain K:'), disp(K)
 
 % Call video function
 addpath(pwd, 'SimulationVideo')
